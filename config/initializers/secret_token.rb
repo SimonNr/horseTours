82590a377b75c4db2ac7404cad06b1ba5a703b2c
Simon <sm287415@fh-muenster.de>
2013-01-11 17:10:27 +0100

    Initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HorseTours::Application.config.secret_token = 'c774d99bc8e0446779b637003b2bc8bce3723b86f1ad9eb84c8ce511bd6ebcb95729b34ce4d28dd1aecb7aef48dbc5d8a842c3c2a09a275cbf926a056b5a6e63'
