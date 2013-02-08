858cbba69a58cabfe63d1fb9380b6fff22c22114
Simon <sm287415@fh-muenster.de>
2013-02-08 13:32:32 +0100

    Added smaller stuff

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,3 +1,9 @@
 class Tour < ActiveRecord::Base
   attr_accessible :days, :description, :difficulty, :hours, :minutes, :teaser, :title, :url
+
+  has_many :tour_categories
+  has_many :categories, through: :tour_categories
+
+  validates :description, :difficulty, :teaser, :title, :url, presence: true
+  validates :url, format: %r|^http(s?)://maps.google.de/|
 end
