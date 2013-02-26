a54bcccd356965f8f3bc806a6e0f755de6610e28
Simon <sm287415@fh-muenster.de>
2013-02-26 11:10:31 +0100

    123

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -57,6 +57,7 @@ class TournamentsController < ApplicationController
   # PUT /tournaments/1.json
   def update
     @tournament = Tournament.find(params[:id])
+    @tour.creatorID = current_user.id
 
     respond_to do |format|
       if @tournament.update_attributes(params[:tournament])
