dc61f07207fb9d43c8de7dc3970a59581deb7e2a
Joyce <pferdejoyce@web.de>
2013-02-26 12:23:43 +0100

    add div 'topMenue' in menue

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,7 @@
 class Tournament < ActiveRecord::Base
   attr_accessible :category, :creatorID, :deadline, :endDate, :information, :place, :registrationOffice, :startDate, :title
+
+
+  validates :title, :category, :place, :registrationOffice, :information, presence: true 
+  validates :place, format: %r|^http(s?)://maps.google.de/| 
 end
