f04328f89452fc11d6533aa9ddf4eed6ab629253
Joyce <pferdejoyce@web.de>
2013-02-27 15:59:36 +0100

    edit applications.css(comments) Add Comment seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,19 +26,6 @@ a2.save
 t1 = Tour.create(title: 'testTour', teaser: 'testTourTeaser', difficulty: 'low', days: '0', hours: '2', minutes: '0', description: 'testTourDesc', url: 'http://maps.google.de/maps?saddr=L%C3%BCtkenheide,+Westbevern&daddr=Willingrott,+M%C3%BCnster&hl=de&ie=UTF8&sll=52.004976,7.68078&sspn=0.064989,0.198097&geocode=FbTtGQMdRmF2ACkDrVtjkL65RzHBy-bsWUENGw%3BFaSFGQMdJLF1ACnTDSPpCry5RzHlyikSo3LBOw&oq=Willingro&dirflg=w&mra=ls&t=m&z=14')
 t1.creator = user
 t1.save
-<<<<<<< HEAD
-t2 = Tour.create(title: 'EmsTour', teaser: 'Eine kleine Runde durch den Wald', difficulty: 'low', days: '0', hours: '2', minutes: '0', description: 'Ein kleines St&uuml;ck durch den Wald und dann an der Ems entlang.', url: 'http://maps.google.de/maps?saddr=Sickerhook&daddr=52.0222448,7.7293423+to:52.0200773,7.7282513+to:Sickerhook&hl=de&ll=52.024059,7.743344&spn=0.00713,0.024762&sll=52.019821,7.733388&sspn=0.007131,0.024762&geocode=FRvgGQMdQyF2AA%3BFeTLGQMdvvB1ACm5DMxSHby5RzGvHxUl1HmGhQ%3BFW3DGQMde-x1ACmNnO6PG7y5RzF-xFA4eoS8PQ%3BFZvfGQMdHiJ2AA&t=h&dirflg=w&mra=dvme&mrsp=2&sz=16&via=1,2&z=22')
-t2.creator = u
-t2.save
-t3 = Tour.create(title: 'Haus Langen Tour', teaser: 'Eine Strecke an Haus Langen vorbei', difficulty: 'medium', days: '0', hours: '2', minutes: '0', description: 'Von Westbevern nach Haus Langen und dann durch den Wald wieder zur&uuml;ck', url: 'http://maps.google.de/maps?saddr=L%C3%BCtken+Heide&daddr=52.0303059,7.7573445+to:52.025042,7.753466+to:52.0223343,7.7528793+to:52.018713,7.762558+to:L%C3%BCtken+Heide&hl=de&ie=UTF8&ll=52.027347,7.753601&spn=0.007129,0.024762&sll=52.017298,7.760178&sspn=0.003566,0.012381&geocode=FQnxGQMd-l52AA%3BFWHrGQMdIF52ACn1no_7mr65RzF1SGH-MGJ-JA%3BFdLWGQMd-k52ACnjFJZNnL65RzFYzZM9zoiPaA%3BFT7MGQMdr0x2ACmtM8Wbnb65RzH43JiMxRVDiQ%3BFRm-GQMdfnJ2ACnpgUqPKby5RzF0A9he_w5FCg%3BFZ7wGQMdcV12AA&t=h&dirflg=w&mra=mrv&via=1,2,3,4&z=22')
-t3.creator = u
-t3.save
-t4 = Tour.create(title: 'Fredde Tour', teaser: 'Eine kurze spazieren Reitstrecke', difficulty: 'low', days: '0', hours: '1', minutes: '0', description: 'Die Strecke geht gr&ooml;&szlig;tenteils an asphaltierten Stra&szlig;en entlang. Gute f&uuml;r eine kleine Schrittrunde', url: 'http://maps.google.de/maps?saddr=L%C3%BCtken+Heide&daddr=52.0357706,7.7616758+to:52.0380512,7.7690281+to:52.0369898,7.7731793+to:52.0304648,7.761517+to:L%C3%BCtken+Heide&hl=de&ie=UTF8&ll=52.034924,7.76684&spn=0.007128,0.024762&sll=52.031848,7.764287&sspn=0.007129,0.024762&geocode=FQnxGQMd-l52AA%3BFboAGgMdC292ACkVBZsUjr65RzEeS5aVVJMAfQ%3BFaMJGgMdxIt2ACl1s-GIi765RzGmFYUWunyAFQ%3BFX0FGgMd-5t2ACkh6TzVi765RzFJzz7zDKqdow%3BFQDsGQMdbW52ACmxoi1Chb65RzECpdTJD6VXyA%3BFZ7wGQMdcV12AA&t=h&dirflg=w&mra=mrv&via=1,2,3,4&z=22')
-t4.creator = u
-t4.save
-t5 = Tour.create(title: 'Klatenberge Tour', teaser: 'Eine lange Strecke durch die Klatenberge', difficulty: 'high', days: '0', hours: '4', minutes: '0', description: 'Eine lange Strecke. Es geht von Westbevern aus Richtung Klatenberge und von dort wieder zur&uuml;ck.', url: 'http://maps.google.de/maps?saddr=L%C3%BCtken+Heide&daddr=52.0312311,7.7630289+to:52.0216741,7.7646947+to:52.0149199,7.7860839+to:52.000122,7.7901371+to:52.0007276,7.7833382+to:52.0194078,7.7721666+to:52.0309303,7.7650473+to:L%C3%BCtken+Heide&hl=de&ie=UTF8&sll=52.000498,7.784758&sspn=0.014267,0.049524&geocode=FQnxGQMd-l52AA%3BFf_uGQMdVHR2ACmnxRqBhb65RzGMvy6LrkAjvg%3BFarJGQMd1np2ACmxm9CNgb65RzHpNzC94ft4Ew%3BFUevGQMdY852AClLAopx2b25RzFaMorDu7IdZw%3BFXp1GQMdOd52ACmDZtJPwL25RzGfHXN2WJtY_g%3BFdd3GQMdqsN2ACm94v_Sxr25RzFJEFl7Ll_SUg%3BFc_AGQMdBph2ACnlzWhC1b25RzFY5N5KsfT5FQ%3BFdLtGQMdN3x2ACkN24Hmhb65RzEQ2ZB3ViMQkA%3BFZ7wGQMdcV12AA&t=h&dirflg=w&mra=dpe&mrsp=5&sz=15&via=1,2,3,4,5,6,7&z=22')
-t5.creator = u
-=======
 
 t2 = Tour.create(title: 'EmsTour', teaser: 'Eine kleine Runde durch den Wald', difficulty: 'low', days: '0', hours: '2', minutes: '0', description: 'Ein kleines St&uuml;ck durch den Wald und dann an der Ems entlang.', url: 'http://maps.google.de/maps?saddr=Sickerhook&daddr=52.0222448,7.7293423+to:52.0200773,7.7282513+to:Sickerhook&hl=de&ll=52.024059,7.743344&spn=0.00713,0.024762&sll=52.019821,7.733388&sspn=0.007131,0.024762&geocode=FRvgGQMdQyF2AA%3BFeTLGQMdvvB1ACm5DMxSHby5RzGvHxUl1HmGhQ%3BFW3DGQMde-x1ACmNnO6PG7y5RzF-xFA4eoS8PQ%3BFZvfGQMdHiJ2AA&t=h&dirflg=w&mra=dvme&mrsp=2&sz=16&via=1,2&z=16')
 t2.creator = user
@@ -54,7 +41,6 @@ t4.save
 
 t5 = Tour.create(title: 'Klatenberge Tour', teaser: 'Eine lange Strecke durch die Klatenberge', difficulty: 'high', days: '0', hours: '4', minutes: '0', description: 'Eine lange Strecke. Es geht von Westbevern aus Richtung Klatenberge und von dort wieder zur&uuml;ck.', url: 'http://maps.google.de/maps?saddr=L%C3%BCtken+Heide&daddr=52.0312311,7.7630289+to:52.0216741,7.7646947+to:52.0149199,7.7860839+to:52.000122,7.7901371+to:52.0007276,7.7833382+to:52.0194078,7.7721666+to:52.0309303,7.7650473+to:L%C3%BCtken+Heide&hl=de&ie=UTF8&sll=52.000498,7.784758&sspn=0.014267,0.049524&geocode=FQnxGQMd-l52AA%3BFf_uGQMdVHR2ACmnxRqBhb65RzGMvy6LrkAjvg%3BFarJGQMd1np2ACmxm9CNgb65RzHpNzC94ft4Ew%3BFUevGQMdY852AClLAopx2b25RzFaMorDu7IdZw%3BFXp1GQMdOd52ACmDZtJPwL25RzGfHXN2WJtY_g%3BFdd3GQMdqsN2ACm94v_Sxr25RzFJEFl7Ll_SUg%3BFc_AGQMdBph2ACnlzWhC1b25RzFY5N5KsfT5FQ%3BFdLtGQMdN3x2ACkN24Hmhb65RzEQ2ZB3ViMQkA%3BFZ7wGQMdcV12AA&t=h&dirflg=w&mra=dpe&mrsp=5&sz=15&via=1,2,3,4,5,6,7&z=15')
 t5.creator = user
->>>>>>> ca0b02a796d3e1e241818664bb064bb0cd3bca0f
 t5.save
 
 
@@ -82,3 +68,9 @@ to5.save
 
 
 com1 = TourComment.create(user_id: user.id, tour_id: t1.id, comment:"Nice tour. Easy to follow.")
+com2 = TourComment.create(user_id: a1.id, tour_id: t1.id, comment:"Beautiful, never had so much fun.")
+com3 = TourComment.create(user_id: user.id, tour_id: t1.id, comment:"Me too.")
+com4 = TourComment.create(user_id: user.id, tour_id: t4.id, comment:"Very relaxed tour.")
+com5 = TourComment.create(user_id: a2.id, tour_id: t5.id, comment:"Long Tour. But a lot good places to ride.<br>We made a stop at the restaurant ;)")
+com6 = TourComment.create(user_id: user.id, tour_id: t2.id, comment:"Good Tour.")
+com7 = TourComment.create(user_id: user.id, tour_id: t3.id, comment:"Nice tour.")
