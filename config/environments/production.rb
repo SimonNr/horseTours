f047ca6b2e03824ae69d4401e24db9c606b89885
Simon <sm287415@fh-muenster.de>
2013-03-01 16:30:18 +0100

    heroku still ActiveAdmin problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,6 @@ HorseTours::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_optins = { :host => 'horses.heroku.com'}
-
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
@@ -17,7 +15,7 @@ HorseTours::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
